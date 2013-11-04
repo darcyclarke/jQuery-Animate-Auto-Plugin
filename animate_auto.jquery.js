201d825cf645fd05716b68832375ae61ed9dbfd9
Alex Zaslavsky <alex.zaslavsky.1990@gmail.com>
2013-11-03 18:31:02 -0800

    Rewritten so that at the end of the animation, the height/width CSS properties of the actual DOM element are "auto", not the computed pixel based size

--- animate_auto.jquery.js
+++ animate_auto.jquery.js
@@ -9,19 +9,38 @@
 * 
 */
 
-jQuery.fn.animateAuto = function(prop, speed, callback){
-    var elem, height, width;
-    return this.each(function(i, el){
-        el = jQuery(el), elem = el.clone().css({"height":"auto","width":"auto"}).appendTo("body");
-        height = elem.css("height"),
-        width = elem.css("width"),
-        elem.remove();
+jQuery.fn.animateAuto = function(props, speed, callback){
+	var elem, height, width;
+	return this.each(function(i, el){
+		//Create a copy of the element, then measure it's height and width if they were "auto"
+		el = $(el);
+		elem = el.clone().css({"height":"auto","width":"auto"}).appendTo(el.parent());
+		height = elem.css("height");
+		width = elem.css("width");
+		elem.remove();
 
-        if(prop === "height")
-         el.animate({"height":height}, speed, callback);
-        else if(prop === "width")
-          el.animate({"width":width}, speed, callback);  
-        else if(prop === "both")
-          el.animate({"width":width,"height":height}, speed, callback);
-    });  
+		//Wrape the callback in function that ensures the height/width is reset to "auto" after the animation is over
+		var wrappedCallback = function() {
+			//Reset the height/width css property to auto
+			if (prop !== 'both') {
+				el.css(prop, 'auto');
+			} else {
+				el.css({height: 'auto', width: 'auto' });
+			}
+
+			//If there is a passed callback function, fire it
+			if (callback) {
+				callback.call(el);
+			}
+		};
+
+		//Do the animation
+		if (props.height && props.height === 'auto') {
+			props.height = height;
+		}
+		if (props.width && props.width === 'auto') {
+			props.width = width;
+		}
+		el.animate(props, speed, wrappedCallback);
+	});
 }
--- animate_auto.min.jquery.js
+++ animate_auto.min.jquery.js
@@ -1,10 +1,12 @@
 /*!
- *
- * jQuery Animate Auto Values Plugin v.1.0
- * http://darcyclarke.me/development/fix-jquerys-animate-to-allow-auto-values/
- *
- * Copyright 2011, Darcy Clarke
- * Do what you want license
- * 
- */
-jQuery.fn.animateAuto=function(a,b,c){var d,e,f;return this.each(function(g,h){h=jQuery(h),d=h.clone().css({height:"auto",width:"auto"}).appendTo("body");e=d.css("height"),f=d.css("width"),d.remove();if(a==="height")h.animate({height:e},b,c);else if(a==="width")h.animate({width:f},b,c);else if(a==="both")h.animate({width:f,height:e},b,c)})}
+*
+* jQuery animateAuto Plugin v.1.0
+* Based on the original code provided by Darcy Clarke at:
+* http://darcyclarke.me/development/fix-jquerys-animate-to-allow-auto-values/
+*
+* Copyright 2013, Alex Zaslavsky
+* MIT License
+* 
+*/
+
+jQuery.fn.animateAuto=function(e,t,n){var r,i,s;return this.each(function(o,u){u=$(u);r=u.clone().css({height:"auto",width:"auto"}).appendTo(u.parent());i=r.css("height");s=r.css("width");r.remove();var a=function(){if(prop!=="both"){u.css(prop,"auto")}else{u.css({height:"auto",width:"auto"})}if(n){n.call(u)}};if(e.height&&e.height==="auto"){e.height=i}if(e.width&&e.width==="auto"){e.width=s}u.animate(e,t,a)})}
